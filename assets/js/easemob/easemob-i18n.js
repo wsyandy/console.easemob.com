9e02cf7e771a4529319d0fb7301fdc6a8532ffd1
Lynch Lee <Lynch.lee9527@gmail.com>
2015-06-16 20:03:04 +0800

    add resource files for i18n

---
+++ assets/js/easemob/easemob-i18n.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kenshinn on 15-6-16.
+ */
+
+var I18NProps = function(){
+    return {
+
+        // load properties used in every page
+        loadCommon: function(){
+
+        }
+    }
+}();
